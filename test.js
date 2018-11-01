30f0f3cc34bf768884471d0d33a817ccb7b18e70
Brian Fajardo <brian.fajardo@osedea.com>
2018-11-01 11:42:52 -0400

    Create expect testing function

--- test.js
+++ test.js
@@ -3,6 +3,46 @@ const { sum } = require('./math');
 const result = sum(5, 4);
 const expected = 9;
 
-if (result !== expected) {
-  throw new Error(`${result} does not equal ${expected}`);
+expect(result).toEqual(expected); // Throws error!
+expect('hello world').toBeString(); // Pass
+expect(15).toBeNumber(); // Pass
+expect(3).toBeLessThan(5); // Pass
+
+function expect(result) {
+  if (!result) {
+    throw new Error('Test value not provided');
+  }
+
+  return {
+    toEqual(expected) {
+      if (result !== expected) {
+        throw new Error(`${result} is not equal to ${expected}`);
+      }
+    },
+    toNotEqual(expected) {
+      if (result === expected) {
+        throw new Error(`${result} is equal to ${expected}`);
+      }
+    },
+    toBeGreaterThan(expected) {
+      if (result < expected) {
+        throw new Error(`${result} is not greater than ${expected}`);
+      }
+    },
+    toBeLessThan(expected) {
+      if (result > expected) {
+        throw new Error(`${result} is not less than ${expected}`);
+      }
+    },
+    toBeString() {
+      if (typeof result !== 'string') {
+        throw new Error(`${result} is not of type string`);
+      }
+    },
+    toBeNumber() {
+      if (typeof result !== 'number') {
+        throw new Error(`${result} is not of type number`);
+      }
+    },
+  };
 }
