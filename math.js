23d07baf5b75a5e8b3ac3e13af94bad838e31586
Brian Fajardo <brian.fajardo@osedea.com>
2018-11-01 10:44:34 -0400

    Move sum function into its own math module

---
+++ math.js
@@ -0,0 +1,4 @@
+// This function is intentionally broken to see errors in tests.
+const sum = (a, b) => a - b;
+
+module.exports = { sum };
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-const sum = (a, b) => a + b;
+const { sum } = require('./math');
 
 const result = sum(5, 4);
 const expected = 9;
 
 if (result !== expected) {
-    throw new Error(`${result} does not equal ${expected}`)
+  throw new Error(`${result} does not equal ${expected}`);
 }
